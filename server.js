4c73cb51b8871209a75a7ff8ac39cc61927a407b
Swetha-Perumalla <ssperumalla7@gmail.com>
2025-04-15 10:47:06 +0530

    Modification

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error('DB connection error', err));
-
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/tasks', require('./routes/tasks'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -30,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection (removed deprecated options)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('DB connection error', err));
